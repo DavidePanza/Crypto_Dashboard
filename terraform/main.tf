6b9b52a7a273c70e6770dd358c09e5e9675ce343
DavidePanza <davide.panza@gmail.com>
2025-10-13 10:44:44 +0200

    working draft

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_dynamodb_table" "crypto_prices" {
   name           = "crypto-prices"
   billing_mode   = "PAY_PER_REQUEST"  # On-demand pricing
